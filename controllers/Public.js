413b79109f41d0263e90792053664211cd7dee67
yvesnrb <ynrbode@gmail.com>
2018-05-21 23:45:55 -0300

    adicionado rotas publicas

--- controllers/Public.js
+++ controllers/Public.js
@@ -1,5 +1,17 @@
 module.exports = (app) => {
     app.get('/', (req, res, next) => {
         res.render('index');
+    });
+
+    app.get('/servicos', (req, res, next) => {
+        res.render('servicos');
+    });
+
+    app.get('/quem', (req, res, next) => {
+        res.render('quem');
+    });
+
+    app.get('/produto', (req, res, next) => {
+        res.render('produto');
     })
 }
